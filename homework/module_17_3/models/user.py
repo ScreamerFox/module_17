60ff57d0b076d6415520dde38a3abc27a53ab252
Screfox <142076193+ScreamerFox@users.noreply.github.com>
2024-11-18 07:48:57 +0500

    Add files via upload

---
+++ homework/module_17_3/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+from backend.db import Base
+from models import *
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship("Task", back_populates="user")
+
